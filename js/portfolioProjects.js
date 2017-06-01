fb693003979bee4442fd7f39c1af2b0c50f33af9
Anwar Montasir <anwarmontasir@gmail.com>
2017-06-01 16:15:10 -0700

    added images, populated from JS, edited CSS

---
+++ js/portfolioProjects.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var portfolioProjects = [
+  {
+    title:       'Good Project',
+    category:    ['JavaScript', 'jQuery'],
+    projectUrl:  'https://placekitten.com/',
+    projectImg:  '../img/kitten01.jpg',
+    imgCaption:  'a kitten, doing kitten stuff',
+    publishedOn: '2017-05-31',
+    body:        '<p>Text to come.</p>'
+  },
+  {
+    title:       'Great Project',
+    category:    ['HTML5', 'CSS'],
+    projectUrl:  'https://meowmix.com/',
+    projectImg:  '../img/kitten02.jpg',
+    imgCaption:  'a kitten, doing kitten stuff',
+    publishedOn: '2017-05-16',
+    body:        '<p>Text to come.</p>'
+  },
+  {
+    title:       'Mediocre Project',
+    category:    ['JavaScript'],
+    projectUrl:  'https://http.cat/',
+    projectImg:  '../img/kitten03.jpg',
+    imgCaption:  'a kitten, doing kitten stuff',
+    publishedOn: '2017-03-28',
+    body:        '<p>Text to come.</p>'
+  },
+  {
+    title:       'Awesome Project',
+    category:    ['HTML5', 'CSS', 'JavaScript', 'jQuery'],
+    projectUrl:  'http://catversushuman.com/',
+    projectImg:  '../img/kitten04.jpg',
+    imgCaption:  'a kitten, doing kitten stuff',
+    publishedOn: '2016-10-31',
+    body:        '<p>Text to come.</p>'
+  }
+];
