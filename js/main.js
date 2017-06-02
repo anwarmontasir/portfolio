90eca4d8dcd1462d68e1d7bc0436961f4779edae
Anwar Montasir <anwarmontasir@gmail.com>
2017-06-02 16:58:42 -0700

    work on JS for various sections

--- js/main.js
+++ js/main.js
@@ -49,3 +49,42 @@ portfolioProjects.forEach(function(projectObject) {
 projects.forEach(function(project) {
   $('#project-content').append(project.toHtml());
 });
+
+var pageView = {};
+
+pageView.handleMainNav = function () {
+  $('nav ul li a').on('click',function() {
+    event.preventDefault();
+    $('nav ul li a').removeAttr('id', 'current');
+    $(this).attr('id', 'current');
+    var $whereToGo = $(this.hash);
+    console.log($whereToGo);
+    $('.page-section').hide();
+    $($whereToGo).fadeIn(500);
+  }); // Let's now trigger a click on the first .tab element, to set up the page.
+};
+
+pageView.setTeasers = function () {
+  $('.article-body *:nth-of-type(n+2)').hide();
+
+  $('.read-more').on('click', function () {
+    event.preventDefault();
+    $(this).css('display', 'none');
+    $(this).parent().find($('.show-less')).css('display', 'block');
+    $(this).parent().find($('.article-body *:nth-of-type(n+2)')).fadeIn(500);
+
+  });
+  $('.show-less').on('click', function () {
+    event.preventDefault();
+    $(this).css('display', 'none');
+    $(this).parent().find($('.read-more')).css('display', 'block');
+    $(this).parent().find($('.article-body *:nth-of-type(n+2)')).hide();
+
+  });
+
+};
+
+$(document).ready(function () {
+  pageView.handleMainNav();
+  pageView.setTeasers();
+});
