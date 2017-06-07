27e2d2b6188c523dc009bcdb7047d1e533c45a8b
Anwar Montasir <anwarmontasir@gmail.com>
2017-06-06 17:29:04 -0700

    content comes from handlebars.js template

--- js/projects.js
+++ js/projects.js
@@ -13,26 +13,12 @@ function Project (portfolioProjectsObj) {
 }
 
 Project.prototype.toHtml = function() {
-  var $newProject = $('article.template').clone();
-  $newProject.removeClass('template');
-
-  if (!this.publishedOn) $newProject.addClass('draft');
-  $newProject.data('category', this.category);
-
-  /* $newArticle.find('address a').html(this.author);
-  $newArticle.find('address a').attr('href', this.authorUrl); */
-  $newProject.find('h2').html(this.title);
-  $newProject.find('img').attr({
-    'src': this.projectImg,
-    'alt': this.imgCaption
-  });
-  $newProject.find('.article-body').html(this.body);
-  $newProject.find('.project-link').attr('href', this.projectUrl);
-  $newProject.find('time').attr('datetime', this.publishedOn);
-  // Display the date as a relative number of 'days ago'
-  var daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-  $newProject.find('time').html('about ' + daysAgo + ' days ago.');
-  return $newProject;
+  var templateFiller = Handlebars.compile( $('#project-template').html());
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
+  var filledTemplate = templateFiller(this);
+  console.log('filledTemplate' + filledTemplate);
+  return filledTemplate;
 };
 
 portfolioProjects.sort(function(a,b) { // eslint-disable-line
