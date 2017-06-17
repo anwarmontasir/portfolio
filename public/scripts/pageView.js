5a6600adf7031e0c822fe8a06ddc3fa444a83be5
Anwar Montasir <anwarmontasir@gmail.com>
2017-06-17 16:05:48 -0700

    wrapping scripts in IIFEs

--- public/scripts/pageView.js
+++ public/scripts/pageView.js
@@ -1,41 +1,49 @@
-var pageView = {};
+'use strict';
 
-pageView.handleMainNav = function () {
-  $('nav ul li a').on('click', function () {
-    event.preventDefault();
-    $('nav ul li a').removeAttr('id', 'current');
-    $(this).attr('id', 'current');
-    var $whereToGo = $(this.hash);
-    console.log($whereToGo);
-    $('.page-section').fadeOut('fast');
-    $($whereToGo).fadeIn('slow');
-  }); // Let's now trigger a click on the first .tab element, to set up the page.
-};
+var app = app || {};
 
-pageView.setTeasers = function () {
-  $('.article-body *:nth-of-type(n+2)').hide();
+(function (module) {
+  const pageView = {};
 
-  $('.read-more').on('click', function () {
-    event.preventDefault();
-    $(this).css('display', 'none');
-    $(this).parent().find($('.show-less')).css('display', 'block');
-    $(this).parent().find($('.article-body *:nth-of-type(n+2)')).fadeIn(500);
+  pageView.handleMainNav = function () {
+    $('nav ul li a').on('click', function () {
+      event.preventDefault();
+      $('nav ul li a').removeAttr('id', 'current');
+      $(this).attr('id', 'current');
+      var $whereToGo = $(this.hash);
+      console.log($whereToGo);
+      $('.page-section').fadeOut('fast');
+      $($whereToGo).fadeIn('slow');
+    }); // Let's now trigger a click on the first .tab element, to set up the page.
+  };
 
-  });
-  $('.show-less').on('click', function () {
-    event.preventDefault();
-    $(this).css('display', 'none');
-    $(this).parent().find($('.read-more')).css('display', 'block');
-    $(this).parent().find($('.article-body *:nth-of-type(n+2)')).hide();
+  pageView.setTeasers = function () {
+    $('.article-body *:nth-of-type(n+2)').hide();
 
-  });
+    $('.read-more').on('click', function () {
+      event.preventDefault();
+      $(this).css('display', 'none');
+      $(this).parent().find($('.show-less')).css('display', 'block');
+      $(this).parent().find($('.article-body *:nth-of-type(n+2)')).fadeIn(500);
 
-};
+    });
+    $('.show-less').on('click', function () {
+      event.preventDefault();
+      $(this).css('display', 'none');
+      $(this).parent().find($('.read-more')).css('display', 'block');
+      $(this).parent().find($('.article-body *:nth-of-type(n+2)')).hide();
 
-pageView.initIndexPage = function () {
-  Project.all.forEach(function (project) { // eslint-disable-line
-    $('#project-content').append(project.toHtml());
-  });
-  pageView.handleMainNav();
-  pageView.setTeasers();
-};
+    });
+
+  };
+
+  pageView.initIndexPage = function () {
+    Project.all.forEach(function (project) { // eslint-disable-line
+      $('#project-content').append(project.toHtml());
+    });
+    pageView.handleMainNav();
+    pageView.setTeasers();
+  };
+
+  module.pageView = pageView;
+}(app));
--- server.js
+++ server.js
@@ -1,26 +1,18 @@
 'use strict';
 
-// Initialize your project using NPM to create and populate a package.json file
+const pg = require('pg');
+const fs = require('fs');
 const express = require('express');
-// instantiate express so we can use its functionality
-const app = express();
-// Require the Express package that you installed via NPM, and instantiate the app
-// Remember to install express, and be sure that it's been added to your package.json as a dependency
-// There is also a package here called body-parser, which is required in for use in a new route.
-// Be sure to install that and save it as a dependency after you create your package.json.
-const bodyParser = require('body-parser').urlencoded({extended: true});
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
+const app = express();
+const conString = 'postgres://localhost:5432/portfolio'
+const client = new pg.Client(conString);
+client.connect();
+client.on('error', function(err) {
+  console.error(err);
+});
 
-// Include all of the static resources as an argument to app.use()
-
-// define which directory we will serve our files from
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('./public'));
-
-app.get('*', function(request, response) {
-  response.status('404').sendFile('/public/404.html', {root: '.'});
-});
-
-app.listen(PORT, function() {
-  // DONE: Log to the console a message that lets you know which port your server has started on
-  console.log(`listening on ${PORT}`);
-});
